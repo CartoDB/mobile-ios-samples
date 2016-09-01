b45dede23b98dffebcb385281337977abd8104ba
Aare Undo <aare@carto.com>
2016-09-01 18:31:50 +0300

    CartoMap

--- CartoMap.Objective-C/CartoMap.Objective-C/AppDelegate.h
+++ CartoMap.Objective-C/CartoMap.Objective-C/AppDelegate.h
@@ -1,17 +1,8 @@
-//
-//  AppDelegate.h
-//  CartoMap.Objective-C
-//
-//  Created by Aare Undo on 01/09/16.
-//  Copyright © 2016 Aare Undo. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
+@property (strong, nonatomic) UINavigationController *navigationController;
 
 @end
-
--- CartoMap.Objective-C/CartoMap.Objective-C/AppDelegate.m
+++ CartoMap.Objective-C/CartoMap.Objective-C/AppDelegate.m
@@ -1,45 +1,24 @@
-//
-//  AppDelegate.m
-//  CartoMap.Objective-C
-//
-//  Created by Aare Undo on 01/09/16.
-//  Copyright © 2016 Aare Undo. All rights reserved.
-//
-
 #import "AppDelegate.h"
-
-@interface AppDelegate ()
-
-@end
+#import "LauncherListController.h"
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    [NTMapView registerLicense:@"XTUMwQ0ZIcXhuZ3RSdWdYNllQM25tRng2Q0tacGJGY2dBaFVBZ2N3K2dFTEFXRWx3OVdzbVF4aC90OVQxMEJJPQoKcHJvZHVjdHM9c2RrLWlvcy0zLioKYnVuZGxlSWRlbnRpZmllcj1jb20uY2FydG8uKgp3YXRlcm1hcms9Y3VzdG9tCnZhbGlkVW50aWw9MjAxNi0xMi0wMQp1c2VyS2V5PTM2M2YxNzdjZjM2NTAzMGYxZWU4YjkzY2JmNTY3OGRhCg"];
+    
+    [NTLog setShowDebug:YES];
+    [NTLog setShowInfo:YES];
+    
+    LauncherListController* controller = [[LauncherListController alloc] init];
+    
+    self.navigationController = [[UINavigationController alloc] initWithRootViewController: controller];
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    [self.window setRootViewController: self.navigationController];
+    [self.window makeKeyAndVisible];
+    
     return YES;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
