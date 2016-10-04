67782c9ef8577636d3b7a5efc74847df66e79b22
Aare Undo <aare@carto.com>
2016-10-04 17:26:46 +0300

    AdvancedMap.Objective-C: test cleanup, controller loop logic

--- AdvancedMap.Objective-C/AdvancedMap.Objective-C.UITests/AdvancedMap_Objective_C_UITests.m
+++ AdvancedMap.Objective-C/AdvancedMap.Objective-C.UITests/AdvancedMap_Objective_C_UITests.m
@@ -44,50 +44,20 @@
     XCUIElementQuery *tablesQuery = self.app.tables;
     XCUIElementQuery* cells = tablesQuery.element.cells;
 
-    XCUIElement* element = [cells elementBoundByIndex:0];
-    [element tap];
-    
-//    for (int i = 0; i < count; i++) {
-//        XCUIElement* element = [cells elementBoundByIndex:i];
-//        [element tap];
-//    }
-    
+    for (int i = 0; i < [cells count]; i++) {
+        
+//        if (i > 0) {
+            // TODO -> && [cells elementBoundByIndex:i - 1] frame.bottom ~= self.app.frame.bottom
+//            [self.app swipeUp];
+//        }
+        
+        XCUIElement* element = [cells elementBoundByIndex:i];
+        [element tap];
+        
+        XCUIElement* backButton = [self.app.buttons elementBoundByIndex:0];
+        [backButton tap];
+    }
 }
-
-//- (void)testExample2 {
-//    
-//    UITableView* table = (UITableView *)self.controller.view;
-
-//    NSString* sampleCount = [NSString stringWithFormat:@"%lu", (unsigned long)[self.controller.samples count]];
-//    NSLog(@"SAMPLES COUNT: ");
-//    NSLog(sampleCount);
-    
-//    NSLog(@"SUBVIEW COUNT: ");
-//    UITableView* child = (UITableView *)table.subviews[0];
-//    NSString *subviewCount = [NSString stringWithFormat:@"%lu", [child.subviews count]];
-//    NSLog(subviewCount);
-    
-//    XCUIElementQuery* query = [self.app.cells containingType:XCUIElementTypeStaticText identifier:@"MapListCell"];
-//    NSLog(@"QUERY COUNT: ");
-//    NSString *queryCount = [NSString stringWithFormat:@"%lu", [query count]];
-//    NSLog(queryCount);
-    
-//    NSUInteger count = self.controller.samples.count;
-//    CGFloat cellheight = 70;
-//    CGFloat width = table.frame.size.width;
-//    
-//    CGFloat y = 10;
-//    
-//    for (int i = 1; i <= count; i++) {
-//        CGVector point = CGVectorMake(width / 2, y);
-//        XCUICoordinate* coordinate = [self.app coordinateWithNormalizedOffset:point];
-//        
-//        sleep(2);
-//        [coordinate tap];
-//        
-//        y += cellheight;
-//    }
-//}
 
 @end
 
