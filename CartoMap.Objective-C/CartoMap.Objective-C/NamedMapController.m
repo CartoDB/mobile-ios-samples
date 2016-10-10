a26ab0d27a896abfc026a5220433f3c755095722
Jaak <jaak@nutiteq.com>
2016-10-10 17:36:55 +0300

    namedmap with onpremises

---
+++ CartoMap.Objective-C/CartoMap.Objective-C/NamedMapController.m
@@ -0,0 +1,9 @@
+//
+//  NamedMapController.m
+//  CartoMap.Objective-C
+//
+//  Created by Jaak Laineste on 10/10/16.
+//  Copyright © 2016 Aare Undo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
