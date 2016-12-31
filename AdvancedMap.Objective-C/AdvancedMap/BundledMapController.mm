bf262fc5936c3208307917197b4d81ce5d0040c5
Jaak <jaak@nutiteq.com>
2016-12-31 19:18:23 +0200

    no need to copy bundled mbtiles in ios

--- AdvancedMap.Objective-C/AdvancedMap/BundledMapController.mm
+++ AdvancedMap.Objective-C/AdvancedMap/BundledMapController.mm
@@ -16,7 +16,8 @@
 	[super viewDidLoad];
     
     // Remove default baselayer
-    [[self.mapView getLayers] clear];
+   // [[self.mapView getLayers] clear];
+    
     
 	// Get the base projection set in the base class
 	NTProjection* projection = [[self.mapView getOptions] getBaseProjection];
@@ -27,32 +28,34 @@
     NTZippedAssetPackage *package = [[NTZippedAssetPackage alloc]initWithZipData:styleAsset];
     NTCompiledStyleSet *styleSet = [[NTCompiledStyleSet alloc]initWithAssetPackage:package];
     
-    NTMBVectorTileDecoder *decoder = [[NTMBVectorTileDecoder alloc]initWithCompiledStyleSet:styleSet];
+//    NTMBVectorTileDecoder *decoder = [[NTMBVectorTileDecoder alloc]initWithCompiledStyleSet:styleSet];
 
+    NTCartoCSSStyleSet* stylesheet = [[NTCartoCSSStyleSet alloc]initWithCartoCSS:@"#agri_landsuit_rice_th_all_suit_iusegeojson{ line-color: #FFFFFF; line-width: 0; } #agri_landsuit_rice_th_all_suit_iusegeojson[suit_type='S1'] { polygon-fill: #269B00; polygon-opacity: 0.5; } #agri_landsuit_rice_th_all_suit_iusegeojson[suit_type='S2'] { polygon-fill: #FFFF00; polygon-opacity: 0.5; }"];
+    
+    NTMBVectorTileDecoder *decoder = [[NTMBVectorTileDecoder alloc]initWithCartoCSSStyleSet:stylesheet];
+    
     NTVectorTileLayer *layer = [[NTVectorTileLayer alloc]initWithDataSource:source decoder:decoder];
     [[self.mapView getLayers]add:layer];
     
     // Zoom to Rome
     [self.mapView setFocusPos:[projection fromWgs84:[[NTMapPos alloc] initWithX:12.4807 y: 41.8962]]  durationSeconds:0];
     [self.mapView setZoom:13 durationSeconds:0];
+    
+    NTMapPos* position = [projection fromLat:8.11065195 lng:99.89845797];
+    [self.mapView setFocusPos:position durationSeconds:0];
+    [self.mapView setZoom:11 durationSeconds:0];
+    
 }
 
 - (NTTileDataSource*)createTileDataSource
 {
-    NSString *name = @"rome_ntvt";
+    NSString *name = @"agri_landsuit_rice_th_all_suit_iuse";
     NSString *extension = @"mbtiles";
     
     // file-based local offline datasource
     NSString* source = [[NSBundle mainBundle] pathForResource:name ofType:extension];
     
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
-    NSString *destination = [paths firstObject];
-    
-    if ([[NSFileManager defaultManager] isReadableFileAtPath:source]) {
-        [[NSFileManager defaultManager] copyItemAtPath:source toPath:destination error:nil];
-    }
-    
-    NTTileDataSource* vectorTileDataSource = [[NTMBTilesTileDataSource alloc] initWithMinZoom:0 maxZoom:14 path:destination];
+    NTTileDataSource* vectorTileDataSource = [[NTMBTilesTileDataSource alloc] initWithMinZoom:0 maxZoom:14 path:source];
     return vectorTileDataSource;
 }
 
