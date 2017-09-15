b9fd5e6965d9e811ece7754ef125297701a1222a
Aare Undo <aare@carto.com>
2017-09-15 14:25:54 +0300

    Advanced.Obj: bridging headers for swift views

--- AdvancedMap.Objective-C/AdvancedMap/LauncherListController.mm
+++ AdvancedMap.Objective-C/AdvancedMap/LauncherListController.mm
@@ -1,4 +1,5 @@
 #import "LauncherListController.h"
+#import "AdvancedMap_Objective_C-Swift.h"
 
 #define RGB(r, g, b) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1]
 #define IsHeader(sample) [[sample objectForKey:@"controller"] rangeOfString:@"Header"].location != NSNotFound
