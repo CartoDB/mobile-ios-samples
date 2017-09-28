d735a5ffde14ab960a0300dc60626010acd9ccef
Aare Undo <aare@carto.com>
2017-09-28 15:15:57 +0300

    Carto.Obj: deleted unused files

--- CartoMap.Objective-C/CartoMap.Objective-C/LauncherListController.h
+++
@@ -1,9 +0,0 @@
-
-#import <CartoMobileSDK/CartoMobileSDK.h>
-
-/*
- * A controller for displaying sample list and launching the selected sample controller.
- */
-@interface LauncherListController : UITableViewController<UITableViewDelegate, UITableViewDataSource>
-
-@end
--- CartoMap.Objective-C/CartoMap.Objective-C/TilePackagerController.m
+++
@@ -1,9 +0,0 @@
-//
-//  TilePackagerController.m
-//  CartoMap.Objective-C
-//
-//  Created by Aare Undo on 27/10/16.
-//  Copyright © 2016 Aare Undo. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
