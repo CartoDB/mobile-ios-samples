4d07763b3a33db8106779bc5e111c260ad10a9f5
Aare Undo <aare@carto.com>
2017-10-09 11:24:25 +0300

    Advanced.Swift: refactored location marker to be its own object

---
+++ AdvancedMap.Swift/Feature Demo/LocationMarker.swift
@@ -0,0 +1,103 @@
+//
+//  LocationMarker.swift
+//  AdvancedMap.Swift
+//
+//  Created by Aare Undo on 09/10/2017.
+//  Copyright © 2017 CARTO. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationMarker: NSObject {
+    
+    var map: NTMapView!
+    
+    var source: NTLocalVectorDataSource!
+    var projection: NTProjection!
+    
+    init(mapView: NTMapView) {
+        super.init()
+        self.map = mapView
+        
+        projection = self.map.getOptions().getBaseProjection()
+        source = NTLocalVectorDataSource(projection: projection)
+        let layer = NTVectorLayer(dataSource: source)
+        map.getLayers().add(layer)
+    }
+    
+    var userMarker: NTPoint!
+    var accuracyMarker: NTPolygon!
+    
+    func showAt(location: CLLocation) {
+        
+        let latitude = Double(location.coordinate.latitude)
+        let longitude = Double(location.coordinate.longitude)
+        let accuracy = Float(location.horizontalAccuracy)
+        
+        let position = projection?.fromWgs84(NTMapPos(x: longitude, y: latitude))
+        
+        map.setFocus(position, durationSeconds: 1)
+        map.setZoom(16, durationSeconds: 1)
+        
+        let builder = NTPolygonStyleBuilder()
+        builder?.setColor(Colors.lightTransparentAppleBlue.toNTColor())
+        let borderBuilder = NTLineStyleBuilder()
+        borderBuilder?.setColor(Colors.darkTransparentAppleBlue.toNTColor())
+        borderBuilder?.setWidth(1)
+        
+        builder?.setLineStyle(borderBuilder?.buildStyle())
+        
+        let points = getCirclePoints(latitude: latitude, longitude: longitude, accuracy: accuracy)
+        
+        if (accuracyMarker == nil) {
+            accuracyMarker = NTPolygon(poses: points, holes: NTMapPosVectorVector(), style: builder?.buildStyle())
+            source.add(accuracyMarker)
+        } else {
+            accuracyMarker.setStyle(builder?.buildStyle())
+            accuracyMarker.setGeometry(NTPolygonGeometry(poses: points))
+        }
+        
+        if (userMarker == nil) {
+            let builder = NTPointStyleBuilder()
+            builder?.setColor(Colors.appleBlue.toNTColor())
+            builder?.setSize(16.0)
+            
+            userMarker = NTPoint(pos: position, style: builder?.buildStyle())
+            source.add(userMarker)
+        }
+        
+        userMarker.setPos(position)
+    }
+    
+    func getCirclePoints(latitude: Double, longitude: Double, accuracy: Float) -> NTMapPosVector {
+        // Number of points of circle
+        let N = 100
+        let EARTH_RADIUS = 6378137.0
+        
+        let radius = Double(accuracy)
+        
+        let points = NTMapPosVector()
+        
+        for i in stride(from: 0, to: N, by: 1) {
+            
+            let angle = Double.pi * 2 * (Double(i).truncatingRemainder(dividingBy:Double(N))) / Double(N)
+            let dx = radius * cos(angle)
+            let dy = radius * sin(angle)
+            
+            let lat = latitude + (180 / Double.pi) * (dy / EARTH_RADIUS)
+            let lon = longitude + (180 / Double.pi) * (dx / EARTH_RADIUS) / cos(Double(latitude * Double.pi / 180))
+            
+            let point = projection.fromWgs84(NTMapPos(x: lon, y: lat))
+            points?.add(point)
+        }
+        
+        return points!
+    }
+}
+
+
+
+
+
+
