310c3bc194489ca85bc64df92a22f4e8370f3a77
Aare Undo <aare@carto.com>
2017-10-23 14:25:41 +0300

    Advanced.Swift: improved tbt label

--- AdvancedMap.Swift/Feature Demo/TurnByTurnInformationLabel.swift
+++ AdvancedMap.Swift/Feature Demo/TurnByTurnInformationLabel.swift
@@ -10,12 +10,24 @@ import Foundation
 
 class TurnByTurnInformationLabel: UIView {
     
+    let label = UILabel()
+    
     convenience init() {
         self.init(frame: CGRect.zero)
+        
+        addSubview(label)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        
+        label.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
     }
     
+    func update(result: NTRoutingResult) {
+        let distance = result.getTotalDistance()
+        let time = result.getTotalTime()
+        
+        label.text = distance.description + "; " + time.description
+    }
 }
