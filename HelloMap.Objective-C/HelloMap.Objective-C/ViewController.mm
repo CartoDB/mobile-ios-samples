b98697fd0e757e8907abfb91061d290d79a478ea
mtehver <mark.tehver@gmail.com>
2019-05-14 14:52:40 +0300

    Updated HelloMap.ObjC sample

--- HelloMap.Objective-C/HelloMap.Objective-C/ViewController.mm
+++ HelloMap.Objective-C/HelloMap.Objective-C/ViewController.mm
@@ -14,7 +14,7 @@
 
 @interface HelloMapListener : NTMapEventListener
 
-@property NTMarker* marker;
+@property NTLocalVectorDataSource* vectorDataSource;
 
 @end
 
@@ -23,22 +23,34 @@
 - (void)viewDidLoad {
     
     [super viewDidLoad];
-    // minimal map definition code
+
+    // Minimal map definition code follows with some tweaks
     
+    // Smoother UI/animations
+    [self setPreferredFramesPerSecond:60];
+
     // The storyboard has NTMapView connected as a view
     NTMapView* mapView = (NTMapView*) self.view;
     
+    // Set map view options
+    [[mapView getOptions] setZoomGestures:YES]; // enable zooming on clicks/double clicks
+    [[mapView getOptions] setRenderProjectionMode:NT_RENDER_PROJECTION_MODE_SPHERICAL]; // comment this line to switch to default planar mode
+
+    // Use EPSG4326 projection for coordinates. This allows us to use longitude/latitude coordinates directly.
+    NTProjection* proj = [[NTEPSG4326 alloc] init];
+    [[mapView getOptions] setBaseProjection:proj];
+
     // Add base layer
     NTVectorTileLayer* layer = [[NTCartoOnlineVectorTileLayer alloc] initWithStyle:NT_CARTO_BASEMAP_STYLE_POSITRON];
     [[mapView getLayers] add:layer];
     
-    NTProjection* proj = [[mapView getOptions] getBaseProjection];
-    
     // Animate zoom to Tallinn, Estonia
-    NTMapPos* tallinn = [proj fromWgs84:[[NTMapPos alloc] initWithX:24.646469 y:59.426939]];
-    [mapView setZoom:3 durationSeconds:2];
+    NTMapPos* tallinn = [[NTMapPos alloc] initWithX:24.646469 y:59.426939];
     [mapView setFocusPos:tallinn durationSeconds:0];
-    
+    [mapView setRotation:0 durationSeconds:0];
+    [mapView setZoom:3 durationSeconds:0];
+    [mapView setZoom:4 durationSeconds:2];
+
     // Initialize a local vector data source
     NTLocalVectorDataSource* vectorDataSource = [[NTLocalVectorDataSource alloc] initWithProjection:proj];
     
@@ -54,16 +66,26 @@
     NTColor* color = [[NTColor alloc] initWithR:242 g:68 b:64 a:255];
     [markerStyleBuilder setColor:color];
     
-    NTMarkerStyle* sharedMarkerStyle = [markerStyleBuilder buildStyle];
+    NTMarkerStyle* markerStyle = [markerStyleBuilder buildStyle];
     
     // Define position and add the marker to the Datasource (which is already in a Layer and MapView)
-    NTMarker* marker = [[NTMarker alloc] initWithPos:tallinn style:sharedMarkerStyle];
+    NTMarker* marker = [[NTMarker alloc] initWithPos:tallinn style:markerStyle];
     [vectorDataSource add:marker];
     
     // Add simple event listener that changes size and/or color on map click
-    HelloMapListener* listener = [[HelloMapListener alloc]init];
-    listener.marker = marker;
+    HelloMapListener* listener = [[HelloMapListener alloc] init];
+    listener.vectorDataSource = vectorDataSource;
     [mapView setMapEventListener: listener];
+}
+
+-(void) viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    
+    // The storyboard has NTMapView connected as a view
+    NTMapView* mapView = (NTMapView*) self.view;
+
+    // Disconnect all listeners at this point to avoid leaks
+    [mapView setMapEventListener:nil];
 }
 
 @end
@@ -71,39 +93,26 @@
 
 @implementation HelloMapListener
 
--(void) onMapClicked:(NTMapClickInfo *)mapClickInfo
-{
-    NTMarkerStyleBuilder* builder = [[NTMarkerStyleBuilder alloc] init];
+-(void) onMapClicked:(NTMapClickInfo *)mapClickInfo {
+    // Create a new marker at the clicked position. Use random properties.
+    NTMarkerStyleBuilder* markerStyleBuilder = [[NTMarkerStyleBuilder alloc] init];
     
-    int size = arc4random_uniform(50);
-    [builder setSize:size];
+    int size = arc4random_uniform(20) + 10;
+    [markerStyleBuilder setSize:size];
     
-    NSArray* colors = [self getColors];
-    
+    NSArray* colors = @[
+        [[NTColor alloc]initWithR:255 g:255 b:255 a:255],
+        [[NTColor alloc]initWithR:0 g:0 b:255 a:255],
+        [[NTColor alloc]initWithR:255 g:0 b:0 a:255],
+        [[NTColor alloc]initWithR:0 g:255 b:0 a:255],
+        [[NTColor alloc]initWithR:200 g:120 b:0 a:255]
+    ];
     NTColor* color = [colors objectAtIndex:arc4random_uniform((int)[colors count])];
-    [builder setColor:color];
+    [markerStyleBuilder setColor:color];
     
-    [self.marker setStyle:builder.buildStyle];
-}
-
--(NSArray*) getColors
-{
-    return @[
-             [[NTColor alloc]initWithR:255 g:255 b:255 a:255],
-             [[NTColor alloc]initWithR:0 g:0 b:255 a:255],
-             [[NTColor alloc]initWithR:255 g:0 b:0 a:255],
-             [[NTColor alloc]initWithR:0 g:255 b:0 a:255],
-             [[NTColor alloc]initWithR:0 g:0 b:0 a:255]
-             ];
+    NTMarkerStyle* markerStyle = [markerStyleBuilder buildStyle];
+    NTMarker* marker = [[NTMarker alloc] initWithPos:[mapClickInfo getClickPos] style:markerStyle];
+    [self.vectorDataSource add:marker];
 }
 
 @end
-
-
-
-
-
-
-
-
-
